e240c391224cc440a0a93846d358e9e04b6798e9
jerry <jerry.chen@dfi.com.tw>
2016-07-18 18:16:06 +0800

    init

---
+++ LogHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CommonLibrary
+{
+    class LogHelper
+    {
+        private static string LogPath = System.AppDomain.CurrentDomain.BaseDirectory + @"\Log";
+        private static string sLogFullName = LogPath + @"\" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+        private static object lockMe = new object();
+        public static void Write(string sLogStr)
+        {
+            if (!Directory.Exists(LogPath))
+                Directory.CreateDirectory(LogPath);
+
+            StreamWriter sw;
+            lock (lockMe)
+            {
+                if (!File.Exists(sLogFullName))
+                    sw = File.CreateText(sLogFullName);
+                else
+                    sw = new StreamWriter(sLogFullName, true);
+                sw.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " || " + sLogStr);
+                sw.Close();
+            }
+        }
+
+    }
+}
---
+++ XmlHelper.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CommonLibrary
+{
+    public class XmlHelper
+    {
+        XmlDocument doc;
+
+        public string Load(string Path)
+        {
+            doc = new XmlDocument();
+            try
+            {
+                doc.Load(Path);
+            }
+            catch (Exception)
+            {
+                LogHelper.Write("[MainForm]Xml file load error!!");
+            }
+            return doc.InnerXml;
+        }
+
+        public IList Read(string Path)
+        {
+            return Read(Path, null);
+        }
+        public IList Read(string Path, string RootNode)
+        {
+            if (string.IsNullOrEmpty(RootNode))
+                RootNode = "/";
+            doc = new XmlDocument();
+            try
+            {
+                doc.Load(Path);
+            }
+            catch (Exception e)
+            {
+                LogHelper.Write("[MainForm]Xml file load error!!");
+            }
+            XmlNodeList nodes = doc.SelectNodes(RootNode);
+            return (IList)ResolveNode(nodes);
+        }
+
+        public IList ReadXml(string XmlStr, string XPath)
+        {
+            if (string.IsNullOrEmpty(XPath))
+                XPath = "/";
+            doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(XmlStr);
+            }
+            catch (Exception)
+            {
+                LogHelper.Write("[MainForm]Read XmlString error!!");
+                return null;
+            }
+            XmlNodeList nodes = doc.SelectNodes(XPath);
+            return (IList)ResolveNode(nodes);
+        }
+
+        public string ExtractXml(string XmlStr, string XPath)
+        {
+            if (string.IsNullOrEmpty(XPath))
+                return XmlStr;
+
+            doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(XmlStr);
+            }
+            catch (Exception)
+            {
+                LogHelper.Write("[MainForm]Extract Xml String error!!");
+            }
+            XmlNodeList nodes = doc.SelectNodes(XPath);
+            string xml = "";
+            foreach (XmlNode node in nodes)
+                xml += node.InnerXml;
+            return xml;
+        }
+
+        private object ResolveNode(XmlNodeList nodes)
+        {
+            List<object> NodeList = new List<object>();
+            foreach (XmlNode node in nodes)
+            {
+                //TODO:處理屬性
+                //XmlElement element = (XmlElement)node; 
+                string NodeName = node.Name;
+                object NodeValue;
+                if (node.HasChildNodes)
+                    NodeValue = ResolveNode(node.ChildNodes);
+                else
+                    return node.Value;
+                NodeList.Add(new KeyValuePair<string, object>(NodeName, NodeValue));
+            }
+            return NodeList;
+        }
+
+        public IList Find(IList TargetList, string TargetValue)
+        {
+            foreach (KeyValuePair<string, object> item in TargetList)
+            {
+                if (item.Value.GetType() == typeof(string))
+                {
+                    if (item.Value.ToString() == TargetValue)
+                        return (IList)TargetList;
+                }
+                else
+                {
+                    return Find((IList)item.Value, TargetValue);
+                }
+            }
+            return new List<object>();
+        }
+
+    }
+}
