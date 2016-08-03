9e02d249c02382d599497fc4262375462c9978e7
wei6220 <wei6220@gmail.com>
2016-08-03 18:01:11 +0800

    test iii

--- IniHelper.cs
+++ IniHelper.cs
@@ -22,7 +22,8 @@ namespace CommonLibrary
         /// <returns></returns>
         public static string GetConfigSetting(string key)
         {
-            return ConfigurationManager.AppSettings[key];
+            //return ConfigurationManager.AppSettings[key];
+            return "OK";
         }
         /// <summary>
         /// 取得初始設定檔路徑(預設key:IniPath)
