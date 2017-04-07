f42b710e56552e08642b22a2bbef4484bf1c2e96
jerry <jerry.chen@dfi.com>
2017-04-07 17:56:14 +0800

    revise mailhelper 1.sned() 新增bcc參數 2.設定檔新增測試寄發人員[MAIL_TEST_TO]、預設BCC[MAIL_DEFAULT_BCC] 3.調整測試寄發內容與相關收件者

--- MailUtility/IMailHelper.cs
+++ MailUtility/IMailHelper.cs
@@ -5,5 +5,6 @@ namespace CommonLibrary.MailUtility
     public interface IMailHelper
     {
         void Send(string subject, string content, List<string> toList, List<string> ccList);
+        void Send(string subject, string content, List<string> toList, List<string> ccList, List<string> bccList);
     }
 }
--- MailUtility/MailHelper.cs
+++ MailUtility/MailHelper.cs
@@ -21,7 +21,22 @@ namespace CommonLibrary.MailUtility
             smtpSection = "SMTP";
         }
 
+        public void SetSmtpClient(SmtpClient smtp)
+        {
+            this.smtp = smtp;
+        }
+
+        public void SetFormAddress(string address, string alias)
+        {
+            this.formAddress = new MailAddress(address, alias);
+        }
+
         public void Send(string subject, string content, List<string> toList, List<string> ccList)
+        {
+            Send(subject, content, toList, ccList, null);
+        }
+
+        public void Send(string subject, string content, List<string> toList, List<string> ccList, List<string> bccList)
         {
             try
             {
@@ -35,12 +50,27 @@ namespace CommonLibrary.MailUtility
                     mail.To.Add(string.Join(",", toList.Where(e => !string.IsNullOrEmpty(e)).ToArray()));
                     if (ccList != null)
                         mail.CC.Add(string.Join(",", ccList.Where(e => !string.IsNullOrEmpty(e)).ToArray()));
+                    if (bccList != null)
+                        mail.Bcc.Add(string.Join(",", bccList.Where(e => !string.IsNullOrEmpty(e)).ToArray()));
 
-                    mail.Subject = (IniHelper.GetValue(_baseSection, "ISTEST") != null && string.Compare(IniHelper.GetValue(_baseSection, "ISTEST"), "true", true) == 0
-                        ? IniHelper.GetValue(_baseSection, "MAIL_PREFIX") : "") + subject;
+                    var defaultBcc = IniHelper.GetValue(_baseSection, "MAIL_DEFAULT_BCC");
+                    if (defaultBcc != null && !string.IsNullOrWhiteSpace(defaultBcc))
+                        mail.Bcc.Add(defaultBcc);
+
+                    //處理測試環境，預設為測試
+                    if (IniHelper.GetValue(_baseSection, "ISTEST") == null || string.Compare(IniHelper.GetValue(_baseSection, "ISTEST"), "false", true) != 0)
+                    {
+                        subject = IniHelper.GetValue(_baseSection, "MAIL_PREFIX") + subject;
+                        content = "<p>To : " + mail.To + "</p><p>CC : " + mail.CC + "</p><p>Bcc : " + mail.Bcc + "</p><hr/>" + content;
+                        mail.To.Clear();
+                        mail.To.Add(IniHelper.GetValue(_baseSection, "MAIL_TEST_TO"));
+                        mail.CC.Clear();
+                        mail.Bcc.Clear();
+                    }
+
+                    mail.Subject = subject;
                     mail.Body = content;
                     mail.IsBodyHtml = true;
-
                     smtp.Send(mail);
                 }
             }
@@ -49,16 +79,6 @@ namespace CommonLibrary.MailUtility
                 LogHelper.Write(e.Message);
                 throw e;
             }
-        }
-
-        public void SetSmtpClient(SmtpClient smtp)
-        {
-            this.smtp = smtp;
-        }
-
-        public void SetFormAddress(string address,string alias)
-        {
-            this.formAddress = new MailAddress(address, alias);
         }
 
     }
